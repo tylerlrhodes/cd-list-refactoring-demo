2b253bc80657cc4af455120342ed8c462e525f9b
Tyler Rhodes <tylerlrhodes@gmail.com>
2021-01-24 16:32:00 -0500

    updates to web app..

--- music-list-web-app/src/App.js
+++ music-list-web-app/src/App.js
@@ -1,61 +1,187 @@
 // import logo from './logo.svg';
 import React, { Component } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 const CDListEndpoint = process.env.REACT_APP_CDLISTURL;
 const GetCDs = CDListEndpoint + "/GetCDS"
+const UploadCSVFile = CDListEndpoint + "/UploadCSV"
 
 export class CDListView extends Component {
   constructor(props) {
     super(props)
-    this.state = {cds: []}
-  }
-  async componentDidMount() {
-    var response = await fetch(GetCDs)
-    var json = await response.json()
-    this.setState({cds: json})
-    console.log(this.state)
-    this.state.cds.map((val, idx) => {
-      console.log(idx + " " + val)
-    })
+    this.state = {cds: props.cds || []};
   }
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Artist</th>
-            <th>Title</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.cds.map((val, idx) => {
-            return <tr key={idx}>
-              <td>{val[0]}</td>
-              <td>{val[1]}</td>
-              <td>{val[2]}</td>
-            </tr>
-          })}
-        </tbody>
-      </table>
+      <div className="row">
+        <div className="col-12">
+          <table className="table table-bordered">
+            <thead className="thead-dark">
+              <tr>
+                <th>Artist</th>
+                <th>Title</th>
+                <th>Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.cds.map((val, idx) => {
+                return <tr key={idx}>
+                  <td>{val[0]}</td>
+                  <td>{val[1]}</td>
+                  <td>{val[2]}</td>
+                </tr>
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     )
   };
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Music CD List 1.0
-        </p>
-      </header>
-      <CDListView>
+class CDListManagementForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {artist: '',
+                  title: '',
+                  year: 0};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    switch (target.name) {
+      case "artist":
+        this.setState({artist: target.value});
+        break;
+      case "title":
+        this.setState({title: target.value});
+        break;
+      case "year":
+        this.setState({year: target.value})
+        break;
+      default:
+        break;
+    }
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.artist);
+    console.log(this.state.title);
+    console.log(this.state.year);
+  }
+  async uploadFile(event) {
+    let upload = document.getElementById("uploadFile");
+    if (upload === undefined || upload === null ||
+        upload.files.length === 0) {
+      console.error("No file selected!");
+      return;
+    }
+    var formData = new FormData();
+    formData.append('csvfile', upload.files[0]);
 
-      </CDListView>
-    </div>
-  );
+    await fetch(UploadCSVFile, {
+      method: 'POST',
+      header: {
+        "Content-Type": "text/csv"
+      },
+      body: formData
+    })
+    this.props.onUpload();
+  }
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-6">
+          <hr />
+          <form className="form-horizontal" onSubmit={this.handleSubmit}>
+            <div className="form-row mb-3">
+              <div className="input-group">
+                  <input type="file" className="" id="uploadFile" />
+              </div>
+            </div>
+            <div className="form-row mb-3">
+              <div className = "input-group">
+                  <input type="button" 
+                         value="Upload selected file (replaces list on server)..." 
+                         className=""
+                         onClick={this.uploadFile}></input>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="input-group mb-3">
+                <input type="button" value="Download list as CSV" className=""></input>
+              </div>
+            </div>
+            <hr />
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label>Artist</label>
+                <input type="text" value={this.state.artist} className="form-control" 
+                       name="artist" onChange={this.handleInputChange} />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-5">
+                <label>Title</label>
+                <input type="text" value={this.state.title} className="form-control" 
+                       name="title" onChange={this.handleInputChange} />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-2">
+                <label>Year</label>
+                <input type="text" value={this.state.year} className="form-control" 
+                       name="year" onChange={this.handleInputChange} />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-4">
+                <button type="submit" className="btn btn-primary">Add CD</button>
+              </div>
+            </div>       
+          </form> 
+        </div>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {cds: []}
+    this.getCds = this.getCds.bind(this);
+  }
+  async componentDidMount() {
+    await this.getCds();
+  }
+  async getCds() {
+    var response = await fetch(GetCDs)
+    var json = await response.json()
+    this.setState({cds: json})
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div className="App container">
+        <header className="App-header">
+          <h1>
+            Music CD List 1.0
+          </h1> 
+        </header>
+        <CDListView cds={this.state.cds}>
+        </CDListView>
+        <h3>Add a CD/Upload or Download list:</h3>
+        <CDListManagementForm onUpload={ async () => { await this.getCds() }}
+                              onAddCD={async () => { await this.getCds() }}>
+
+        </CDListManagementForm>
+      </div>
+    );
+  }
 }
 
 export default App;
--- web_app.py
+++ web_app.py
@@ -1,21 +1,88 @@
 """ Web App for Music CD Program """
+
+import os
+import csv
+from pathlib import Path
 from flask import Flask
 from flask import jsonify
+from flask import request
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 from linked_list import LinkedList
 from music_cd import MusicCD
 
+
+
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+
+app.config['UPLOAD_FOLDER'] = Path(UPLOAD_FOLDER)
+
+cds = LinkedList(None)
+cds.add_to_list(MusicCD("Radiohead", "Kid A", 2006, 0))
+cds.add_to_list(MusicCD("Radiohead", "The Bends", 2006, 1))
+cds.add_to_list(MusicCD("Dave Matthews Band", "Under the Table and Dreaming", 2002, 2))
+
+current_csv_file = None
 
 @app.route('/GetCDS')
 def get_cds():
     """ API Endpoint for getting CD List """
-    cds = LinkedList(None)
-    cds.add_to_list(MusicCD("Kid A", "Radiohead", 2006, 0))
-    cds.add_to_list(MusicCD("The Bends", "Radiohead", 2006, 1))
-    cds.add_to_list(MusicCD("Dave Matthews Band", "Under the Table and Dreaming", 2002, 2))
     cd_list = []
     for cd in cds:
         cd_list.append([cd.item.title, cd.item.artist, cd.item.year, cd.item.cdid])
     return jsonify(cd_list)
+
+@app.route('/AddCD', methods = ['POST'])
+def add_cd():
+    """ API Endpoint for adding a CD """
+    try:
+        json = request.get_json()
+        artist = json["artist"]
+        title = json["title"]
+        year = json["year"]
+    except KeyError as e:
+        msg = str(e)
+        return '{"error":"' + msg + '"}'
+
+    cds.add_to_list(MusicCD(artist, title, year, 0))
+
+    return f"{json}"
+
+
+def update_cd_list_from_csv():
+    global cds
+    with open(current_csv_file, newline='') as f:
+        new_list = LinkedList(None)
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            print(row)
+            new_list.add_to_list(MusicCD(row[0], row[1], row[2], 0))
+        cds = new_list
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/UploadCSV', methods=['POST'])
+def upload_file():
+    global current_csv_file
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'csvfile' not in request.files:
+            raise KeyError
+        file = request.files['csvfile']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            raise ValueError
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            current_csv_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(current_csv_file)
+            update_cd_list_from_csv()
+            return jsonify({'Result': True})
