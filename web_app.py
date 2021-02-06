d8aa81d48d763108d499ef4d28bc5abba10d6e58
Tyler Rhodes <tylerlrhodes@gmail.com>
2021-02-06 11:18:25 -0500

    bug fix on web_app.py

--- web_app.py
+++ web_app.py
@@ -2,7 +2,6 @@
 
 import os
 import csv
-import threading
 from pathlib import Path
 from flask import Flask
 from flask import jsonify
@@ -11,6 +10,7 @@ from flask import Response
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from linked_list import LinkedList
+from list_store import ThreadSafeListStore
 from music_cd import MusicCD
 
 app = Flask(__name__)
@@ -21,44 +21,38 @@ ALLOWED_EXTENSIONS = {'csv'}
 
 app.config['UPLOAD_FOLDER'] = Path(UPLOAD_FOLDER)
 
-lock = threading.Lock()
-cds = LinkedList(None)
-cds.add_to_list(MusicCD("Radiohead", "Kid A", 2006, 0))
-cds.add_to_list(MusicCD("Radiohead", "The Bends", 2006, 1))
-cds.add_to_list(MusicCD("Dave Matthews Band", "Under the Table and Dreaming", 2002, 2))
+store = ThreadSafeListStore(LinkedList(None))
+store.append(MusicCD("Radiohead", "Kid A", 2006, 0))
+store.append(MusicCD("Radiohead", "The Bends", 2006, 1))
+store.append(MusicCD("Dave Matthews Band", "Under the Table and Dreaming", 2002, 2))
 
-def lock_it(func):
-    """ Thread Safety for CDs and CSV File... """
-    def wrapper():
-        with lock:
-            return func()
-    wrapper.__name__ = func.__name__
-    return wrapper
+# cds = LinkedList(None)
+# cds.add_to_list(MusicCD("Radiohead", "Kid A", 2006, 0))
+# cds.add_to_list(MusicCD("Radiohead", "The Bends", 2006, 1))
+# cds.add_to_list(MusicCD("Dave Matthews Band", "Under the Table and Dreaming", 2002, 2))
+
 
 @app.route('/GetCDS')
-@lock_it
 def get_cds():
     """ API Endpoint for getting CD List """
     cd_list = []
-    for cd in cds:
-        cd_list.append([cd.item.title, cd.item.artist, cd.item.year, cd.item.cdid])
+    for cd in store:
+        cd_list.append([cd.title, cd.artist, cd.year, cd.cdid])
     return jsonify(cd_list)
 
 @app.route('/DownloadCSV')
-@lock_it
 def download_csv():
     def gen():
         yield 'artist, title, year' + '\n'
         cd_list = []
-        for cd in cds:
-            cd_list.append([cd.item.title, cd.item.artist, str(cd.item.year)])
+        for cd in store:
+            cd_list.append([cd.title, cd.artist, str(cd.year)])
         for row in cd_list:
             yield ','.join(row) + '\n'
     return Response(gen(), mimetype='text/csv', headers={
         "Content-disposition":"attachment; filename=cds.csv"})
 
 @app.route('/AddCD', methods = ['POST'])
-@lock_it
 def add_cd():
     """ API Endpoint for adding a CD """
     try:
@@ -70,28 +64,27 @@ def add_cd():
         msg = str(e)
         return '{"error":"' + msg + '"}'
 
-    cds.add_to_list(MusicCD(artist, title, year, 0))
+    store.append(MusicCD(artist, title, year, 0))
 
     return f"{json}"
 
 
 def update_cd_list_from_csv(current_csv_file):
-    global cds
+    global store
     with open(current_csv_file, newline='') as f:
-        new_list = LinkedList(None)
+        new_list = ThreadSafeListStore(LinkedList(None))
         reader = csv.reader(f)
         next(reader)
         for row in reader:
             print(row)
-            new_list.add_to_list(MusicCD(row[0], row[1], row[2], 0))
-        cds = new_list
+            new_list.append(MusicCD(row[0], row[1], row[2], 0))
+        store = new_list
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/UploadCSV', methods=['POST'])
-@lock_it
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
